2ed0cac8491e376189776f6b1815b75300cdb01b
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-06-10 14:25:17 +0300

    Убрал console log, добавил приведение строки к type number в рандом функциях

--- js/main.js
+++ js/main.js
@@ -3,38 +3,32 @@
 
 function getRandomIntInclusive(min, max) {
   if (max <= min) {
-    // eslint-disable-next-line no-console
-    console.log('Задан неверный диапазон. Максимальное значение должно быть больше минимального.');
-    return;
+    throw new Error('Задан неверный диапазон. Максимальное значение должно быть больше минимального.');
   }
   else if (min < 0) {
     // eslint-disable-next-line no-console
-    console.log('Задан неверный диапазон. Диапазон может быть только положительный, включая ноль.');
-    return;
+    throw new Error('Задан неверный диапазон. Диапазон может быть только положительный, включая ноль.');
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-// eslint-disable-next-line no-console
-console.log(getRandomIntInclusive(2, 5));
+getRandomIntInclusive(2, 5);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
 
 function getRandomInt(min, max, symbolsAfterDot) {
   if (max <= min) {
     // eslint-disable-next-line no-console
-    console.log('Задан неверный диапазон. Максимальное значение должно быть больше минимального.');
-    return;
+    throw new Error('Задан неверный диапазон. Максимальное значение должно быть больше минимального.');
   }
   else if (min < 0) {
     // eslint-disable-next-line no-console
-    console.log('Задан неверный диапазон. Диапазон может быть только положительный, включая ноль.');
-    return;
+    throw new Error('Задан неверный диапазон. Диапазон может быть только положительный, включая ноль.');
   }
-  return (min + Math.random() * (max - min)).toFixed(symbolsAfterDot);
+  return +((min + Math.random() * (max - min)).toFixed(symbolsAfterDot));
 }
 
-// eslint-disable-next-line no-console
-console.log(getRandomInt(2, 5, 3));
+getRandomInt(2, 5, 3);
+
