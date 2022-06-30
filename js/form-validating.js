0e3ef278d6891c1825cd9e63b6a381ab82f086b5
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-06-30 22:10:58 +0300

    преобразовал if в тернарник в validateRoom

--- js/form-validating.js
+++ js/form-validating.js
@@ -7,19 +7,15 @@ const pristine = new Pristine(adForm, {
   errorTextClass: 'ad-form__element--error',
 }, false);
 
-//*GUESTROOMS
+//Валидация roomFieldElement
 const roomFieldElement = adForm.querySelector('#room_number'); // Количество комнат
 const capacityFieldElement = adForm.querySelector('#capacity'); //Количество гостей
 
-//Проверяем соответствие
 const validateRoom = () => {
   const currentRoomValue = parseInt(roomFieldElement.value, 10); //Выбранное количество комнат
   const currentCapacityValue = parseInt(capacityFieldElement.value, 10); //Выбранное количество гостей
 
-  if (currentCapacityValue === 0 || currentRoomValue === 100) {
-    return currentCapacityValue === 0 && currentRoomValue === 100;
-  }
-  return currentRoomValue >= currentCapacityValue;
+  return currentCapacityValue === 0 || currentRoomValue === 100 ? currentCapacityValue === 0 && currentRoomValue === 100 : currentRoomValue >= currentCapacityValue;
 };
 
 pristine.addValidator(roomFieldElement, validateRoom, 'Измените количество комнат/гостей');
