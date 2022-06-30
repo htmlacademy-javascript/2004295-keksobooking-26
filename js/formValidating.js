9da2f71e9ffa56636454f70a5a602df102f1ccec
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-06-30 13:24:50 +0300

    pristine в процессе

--- js/formValidating.js
+++ js/formValidating.js
@@ -1,32 +1,42 @@
 const adForm = document.querySelector('.ad-form');
 
-
 const pristine = new Pristine(adForm, {
-  // class of the parent element where the error/success class is added
   classTo: 'ad-form__element',
-  errorClass: 'ad-form__element--invalid',
-  successClass: 'ad-form__element--valid',
-  // class of the parent element where error text element is appended
   errorTextParent: 'ad-form__element',
-  // type of element to create for the error text
   errorTextTag: 'span',
-  // class of the error text element
-  errorTextClass: 'ad-form__error'
 });
 
 //* TITLE
 function validateTitle (value) {
   return value.length >= 30 && value.length <= 100;
 }
-
 pristine.addValidator(adForm.querySelector('#title'), validateTitle, 'От 30 до 100 символов');
 
 //* PRICE
 function validatePrice (value) {
   return value <= 100000;
 }
-
 pristine.addValidator(adForm.querySelector('#price'), validatePrice, 'Максимум 100000');
+
+//* GUESTROOMS
+
+const type = adForm.querySelector('#type');
+const minPrice = {
+  'bungalow' : 0,
+  'flat' : 1000,
+  'hotel' : 3000,
+  'house' : 5000,
+  'palace' : 10000,
+};
+
+function getMinPrice () {
+  const unit = type.querySelector('[name=“type”]');
+  return minPrice(unit.value);
+}
+
+pristine.addValidator(type, getMinPrice, 'Ошибка');
+
+////*
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
