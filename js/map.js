10124c64be1c50216de1de9bfc467eafffd1b2f1
Mikhail Lavrov <lavrov.fd@gmail.com>
2022-07-06 10:19:37 +0300

    добавил mainPin на карту. Onload

--- js/map.js
+++ js/map.js
@@ -1,8 +1,18 @@
+import {makeFormActive} from './form-toggle.js';
+import {makeFormDisabled} from './form-toggle.js';
+
+makeFormDisabled();
+
+const resetButton = document.querySelector('.ad-form__reset');
+
 const map = L.map('map-canvas')
+  .on('load', () => {
+    makeFormActive();
+  })
   .setView({
-    lat: 59.92749,
-    lng: 30.31127,
-  }, 10);
+    lat: 35.7000,
+    lng: 139.7000,
+  }, 11);
 
 L.tileLayer(
   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -10,3 +20,39 @@ L.tileLayer(
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   },
 ).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 35.7000,
+    lng: 139.7000,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  },
+);
+
+mainPinMarker.addTo(map);
+
+mainPinMarker.on('moveend', (evt) => {
+  const addressElement = document.querySelector('#address');
+  addressElement.value = `${(evt.target.getLatLng().lat).toFixed(5)}, ${(evt.target.getLatLng().lng).toFixed(5)}`;
+});
+
+resetButton.addEventListener('click', () => {
+  mainPinMarker.setLatLng({
+    lat: 35.7000,
+    lng: 139.7000,
+  });
+
+  map.setView({
+    lat: 35.7000,
+    lng: 139.7000,
+  }, 11);
+});
